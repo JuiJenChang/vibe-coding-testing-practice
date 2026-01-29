b152ab7e1b3b136deba7ff03c9954693bfba2f96
changray <raychang@glinteagle.com>
2026-01-29 16:32:04 +0800

    test PR success condition

--- src/pages/__tests__/LoginPage.test.tsx
+++ src/pages/__tests__/LoginPage.test.tsx
@@ -81,7 +81,7 @@ describe('LoginPage', () => {
             fireEvent.change(passwordInput, { target: { value: '12345678' } });
             fireEvent.click(submitButton);
 
-            expect(await screen.findByText('密碼必須包含英文字母和.gh329od90數字')).toBeInTheDocument();
+            expect(await screen.findByText('密碼必須包含英文字母和數字')).toBeInTheDocument();
             expect(mockLogin).not.toHaveBeenCalled();
         });
     });
